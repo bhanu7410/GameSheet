9df1ca1be0ab7756956608ea3b367c32a42f50e9
Bhanu Prabhath <70201186+bhanu7410@users.noreply.github.com>
2025-09-06 17:15:49 +0530

    j

---
+++ app/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Navbar() {
+	return (
+		<>
+			<div className="fixed bottom-0 w-full text-neutral-900 h-25 bg-blue-300 block">
+				Navbar
+			</div>
+		</>
+	);
+}
